1842cc1090b2952c132372972266fd68f4677bb9
brandtnewww.eth <lennart@freshminds.io>
2023-02-21 08:20:32 +0100

    Add index file to hooks.

---
+++ hooks/index.js
@@ -0,0 +1,3 @@
+export { default as useAuthentication } from './useAuthentication'
+export { default as useRemoteConfig } from './useRemoteConfig'
+export { default as useTracking } from './useTracking'
